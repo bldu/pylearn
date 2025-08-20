f92ba0768cf31a921d21e4a16e982ddc86a5a438
dubl <duboluntc@outlook.com>
2025-08-20 17:31:23 +0800

    comment added

--- async_await.py
+++ async_await.py
@@ -1,14 +1,26 @@
+"""
+await 只能在 async def 里用。
+
+asyncio.run() 只能调用一次（不能在已有事件循环里嵌套）。
+
+协程必须用 await 或 asyncio.run() 执行，否则不会真的运行。
+"""
+
 import asyncio
 
 async def foo():
-    print("hi")
+    print("foo()")
 
 async def bar():
+    print("bar() begin")
     await foo()
+    print("bar() end")
 
 def main():
-    asyncio.run(bar())
+    foo() # 只会得到一个 coroutine 对象，不会执行
     asyncio.run(foo())
+    asyncio.run(bar())
+  
 
 
 if __name__ == "__main__":
